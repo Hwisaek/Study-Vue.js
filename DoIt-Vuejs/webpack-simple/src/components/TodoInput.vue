b0d6791d6fec6f49a0ff8770621c90686539572f
이창민 <hwisaek@hyundai-autoever.com>
2021-11-15 19:20:56 +0900

    Do it! Vue.js - ~ 169 page

---
+++ DoIt-Vuejs/webpack-simple/src/components/TodoInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Type what you have to do"
+      @keyup.enter="addTodo"
+    />
+    <span class="addContainer" @click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      // 텍스트가 입력된 경우에만 실행
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim(); // 앞뒤 공백 제거
+        this.$emit("addTodo", value);
+        this.clearInput(); // 입력값 초기화
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6578fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
