9dc07a96d4190ebc6bd0dc546bb38c2f49de19a6
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-11-17 07:57:18 +0900

    20211117_0757 저장

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-header></app-header>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      str: "hi",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
