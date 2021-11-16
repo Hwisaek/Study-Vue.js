00ae636f0bbcbf1420ba16d6d11defcf69218f9b
이창민 <hwisaek@hyundai-autoever.com>
2021-11-16 11:01:54 +0900

    Do it! Vue.js 입문 완료

--- DoIt-Vuejs/todo/src/components/TodoInput.vue
+++ DoIt-Vuejs/todo/src/components/TodoInput.vue
@@ -9,14 +9,28 @@
     <span class="addContainer" @click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false"
+        >할 일을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
+  components: {
+    Modal,
+  },
   data() {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
@@ -26,6 +40,8 @@ export default {
         var value = this.newTodoItem && this.newTodoItem.trim(); // 앞뒤 공백 제거
         this.$emit("addTodo", value);
         this.clearInput(); // 입력값 초기화
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput() {
