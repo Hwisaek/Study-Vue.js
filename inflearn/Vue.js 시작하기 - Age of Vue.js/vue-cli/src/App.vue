707963233fed52152fff5fb69c8a2ebefee0948c
이창민 <hwisaek@hyundai-autoever.com>
2021-11-17 10:14:06 +0900

    완료 - Vue.js 시작하기 - Age of Vue.js

---
+++ inflearn/Vue.js 시작하기 - Age of Vue.js/vue-cli/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">ID</label>
+      <input type="text" name="username" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW</label>
+      <input type="password" name="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
